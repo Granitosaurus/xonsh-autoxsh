b6eef8a13ab8bd9ad9e6f831e8447367e1bf6df2
Bernardas <bernardas.alisauskas@gmail.com>
2016-06-27 13:34:34 +0000

    clean up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='xonsh-autoxsh',
     version='0.1',
-    url='-',
+    url='https://github.com/Granitas/xonsh-autoxsh',
     license='GPLv3',
     author='Bernardas Ališauskas',
     author_email='bernardas.alisauskas@gmail.com',
@@ -11,4 +11,14 @@ setup(
     packages=['xontrib'],
     package_dir={'xontrib': 'xontrib'},
     package_data={'xontrib': ['*.xsh']},
+    platforms='any',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Desktop Environment',
+        'Topic :: System :: Shells',
+        'Topic :: System :: System Shells',
+]
 )
