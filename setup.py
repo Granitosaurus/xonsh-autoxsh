ac6e7b4a3155bd73779584d348a18960f61accde
Bernardas <bernardas.alisauskas@gmail.com>
2016-08-24 10:48:41 +0000

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='xonsh-autoxsh',
-    version='0.1',
+    version='0.2',
     url='https://github.com/Granitas/xonsh-autoxsh',
     license='GPLv3',
     author='Bernardas Ališauskas',
